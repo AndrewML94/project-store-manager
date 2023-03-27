8b0c45cbcc8a1772b16a759d245f13be90824e48
Andrew Lima <andrewmll94@gmail.com>
2023-03-26 21:44:14 -0300

    Fix: Ajuste no lint

--- src/services/validations/schemas.js
+++ src/services/validations/schemas.js
@@ -4,4 +4,4 @@ const nameProduct = Joi.string().min(5).required();
 
 module.exports = {
   nameProduct,
-}
+};
--- src/services/validations/validationsInputValues.js
+++ src/services/validations/validationsInputValues.js
@@ -3,7 +3,9 @@ const { nameProduct } = require('./schemas');
 const validateName = (name) => {
   const { error } = nameProduct.validate(name);
 
-  if (error) return { type: 'INVALID_VALUE', message: '"name" length must be at least 5 characters long' };
+  if (error) {
+    return { type: 'INVALID_VALUE', message: '"name" length must be at least 5 characters long' };
+  }
 
   return { type: null, message: '' };
 };
